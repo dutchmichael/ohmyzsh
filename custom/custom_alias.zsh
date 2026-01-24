891354011ef382b653de11e07e38f667ce2691b9
dutchmichael <m@dutchmichael.com>
2026-01-24 10:30:35 -0500

    updated custom aliases

--- custom/custom_alias.zsh
+++ custom/custom_alias.zsh
@@ -580,7 +580,7 @@ alias rssh='sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist && sud
 
 ##### ----- SSH configuration commands ---- #####
 alias ss='source ~/.ssh/config'
-alias esc='cd ~ && mate --wait ~/.ssh/config && git add -f ~/.ssh/config && git commit -m "updated ssh config" && git push -u git@github.com:/dutchmichael/dotfile_macos.git main && cd -'
+alias esc='cd ~/.ssh && mate --wait ~/.ssh/config && git add -f ~/.ssh/config && git commit -m "updated ssh config" && git push -u origin main && cd -'
 
 # old alias esc='cp ~/.ssh/config ~/.ssh/config.$(date +%y%%d) && mate ~/.ssh/config'
 # alias esc='cp ~/dotfiles_macos/config ~/dotfiles_macos_backup/config.$(date +"%Y%m%d%H%M%S") && mate ~/.ssh/config'
