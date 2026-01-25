2bf1add2a2bc13fb1e14bc0059ddd070ad4f8b85
dutchmichael <m@dutchmichael.com>
2026-01-25 08:44:14 -0500

    updated custom aliases

--- custom/custom_alias.zsh
+++ custom/custom_alias.zsh
@@ -574,8 +574,9 @@ alias csz='chsh -s /bin/zsh'
 alias sr='ssh -p4272 root@192.168.1.1'
 
 ##### Unload and load ssh
-alias rssh='sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist && sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist && echo "reloaded ssh"'
-
+alias rssh='sudo launchctl bootout /System/Library/LaunchDaemons/ssh.plist && sudo launchctl bootstrap -w /System/Library/LaunchDaemons/ssh.plist && echo "reloaded ssh"'
+# sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist
+# sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist 
 # --- Sail | Lavavel ---
 # alias sail='bash vendor/bin/sail'
 
