365433d7af60f7be4b9737257d00ecc0081e9991
dutchmichael <m@dutchmichael.com>
2026-02-01 17:39:28 -0500

    updated custom aliases

--- custom/custom_alias.zsh
+++ custom/custom_alias.zsh
@@ -583,9 +583,6 @@ alias shell='echo $SHELL'
 alias csb='chsh -s /bin/bash'
 alias csz='chsh -s /bin/zsh'
 
-# --- Synology Router ---
-alias sr='ssh -p4272 root@192.168.1.1'
-
 ##### Unload and load ssh
 alias rssh='sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist && sudo launchctl load -wait /System/Library/LaunchDaemons/ssh.plist && echo "reloaded ssh"'
 # sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist
@@ -599,6 +596,9 @@ alias esc='cd ~/.ssh && mate --wait ~/.ssh/config && git add -f ~/.ssh/config &&
 
 # old alias esc='cp ~/.ssh/config ~/.ssh/config.$(date +%y%%d) && mate ~/.ssh/config'
 # alias esc='cp ~/dotfiles_macos/config ~/dotfiles_macos_backup/config.$(date +"%Y%m%d%H%M%S") && mate ~/.ssh/config'
+
+# --- Synology Router ---
+alias sr='ssh -p4272 root@192.168.1.1'
 
 #Sytem profiler command
 alias sp='system_profiler SPSoftwareDataType'
@@ -703,6 +703,138 @@ alias tps='trellis provision staging'
 # alias svh='source /usr/local/etc/httpd/extra/httpd-vhosts.conf'
 # alias lvh='httpd -D DUMP_INCLUDES'
 # alias tvh='httpd -S'
+
+##### ----- VSCodium Commands ----- 
+alias code='codium'
+# codium --help
+# VSCodium 1.108.20787
+#
+# Usage: codium [options] [paths...]
+#
+# To read from stdin, append '-' (e.g. 'ps aux | grep code | codium -')
+#
+# Options
+#   -d --diff <file> <file>                    Compare two files with each
+#                                              other.
+#   -m --merge <path1> <path2> <base> <result> Perform a three-way merge by
+#                                              providing paths for two modified
+#                                              versions of a file, the common
+#                                              origin of both modified versions
+#                                              and the output file to save merge
+#                                              results.
+#   -a --add <folder>                          Add folder(s) to the last active
+#                                              window.
+#   --remove <folder>                          Remove folder(s) from the last
+#                                              active window.
+#   -g --goto <file:line[:character]>          Open a file at the path on the
+#                                              specified line and character
+#                                              position.
+#   -n --new-window                            Force to open a new window.
+#   -r --reuse-window                          Force to open a file or folder in
+#                                              an already opened window.
+#   -w --wait                                  Wait for the files to be closed
+#                                              before returning.
+#   --locale <locale>                          The locale to use (e.g. en-US or
+#                                              zh-TW).
+#   --user-data-dir <dir>                      Specifies the directory that user
+#                                              data is kept in. Can be used to
+#                                              open multiple distinct instances
+#                                              of Code.
+#   --profile <profileName>                    Opens the provided folder or
+#                                              workspace with the given profile
+#                                              and associates the profile with
+#                                              the workspace. If the profile does
+#                                              not exist, a new empty one is
+#                                              created.
+#   -h --help                                  Print usage.
+#
+# Extensions Management
+#   --extensions-dir <dir>              Set the root path for extensions.
+#   --list-extensions                   List the installed extensions.
+#   --show-versions                     Show versions of installed extensions,
+#                                       when using --list-extensions.
+#   --category <category>               Filters installed extensions by provided
+#                                       category, when using --list-extensions.
+#   --install-extension <ext-id | path> Installs or updates an extension. The
+#                                       argument is either an extension id or a
+#                                       path to a VSIX. The identifier of an
+#                                       extension is '${publisher}.${name}'. Use
+#                                       '--force' argument to update to latest
+#                                       version. To install a specific version
+#                                       provide '@${version}'. For example:
+#                                       'vscode.csharp@1.2.3'.
+#   --pre-release                       Installs the pre-release version of the
+#                                       extension, when using
+#                                       --install-extension
+#   --uninstall-extension <ext-id>      Uninstalls an extension.
+#   --update-extensions                 Update the installed extensions.
+#   --enable-proposed-api <ext-id>      Enables proposed API features for
+#                                       extensions. Can receive one or more
+#                                       extension IDs to enable individually.
+#
+# Model Context Protocol
+#   --add-mcp <json> Adds a Model Context Protocol server definition to the user
+#                    profile. Accepts JSON input in the form
+#                    '{"name":"server-name","command":...}'
+#
+# Troubleshooting
+#   -v --version                            Print version.
+#   --verbose                               Print verbose output (implies
+#                                           --wait).
+#   --log <level>                           Log level to use. Default is 'info'.
+#                                           Allowed values are 'critical',
+#                                           'error', 'warn', 'info', 'debug',
+#                                           'trace', 'off'. You can also
+#                                           configure the log level of an
+#                                           extension by passing extension id and
+#                                           log level in the following format:
+#                                           '${publisher}.${name}:${logLevel}'.
+#                                           For example: 'vscode.csharp:trace'.
+#                                           Can receive one or more such
+#                                           entries.
+#   -s --status                             Print process usage and diagnostics
+#                                           information.
+#   --prof-startup                          Run CPU profiler during startup.
+#   --disable-extensions                    Disable all installed extensions.
+#                                           This option is not persisted and is
+#                                           effective only when the command opens
+#                                           a new window.
+#   --disable-extension <ext-id>            Disable the provided extension. This
+#                                           option is not persisted and is
+#                                           effective only when the command opens
+#                                           a new window.
+#   --sync <on | off>                       Turn sync on or off.
+#   --inspect-extensions <port>             Allow debugging and profiling of
+#                                           extensions. Check the developer tools
+#                                           for the connection URI.
+#   --inspect-brk-extensions <port>         Allow debugging and profiling of
+#                                           extensions with the extension host
+#                                           being paused after start. Check the
+#                                           developer tools for the connection
+#                                           URI.
+#   --disable-lcd-text                      Disable LCD font rendering.
+#   --disable-gpu                           Disable GPU hardware acceleration.
+#   --disable-chromium-sandbox              Use this option only when there is
+#                                           requirement to launch the application
+#                                           as sudo user on Linux or when running
+#                                           as an elevated user in an applocker
+#                                           environment on Windows.
+#   --locate-shell-integration-path <shell> Print the path to a terminal shell
+#                                           integration script. Allowed values
+#                                           are 'bash', 'pwsh', 'zsh' or 'fish'.
+#   --telemetry                             Shows all telemetry events which VS
+#                                           code collects.
+#   --transient                             Run with temporary data and extension
+#                                           directories, as if launched for the
+#                                           first time.
+#
+# Subcommands
+#   chat         Pass in a prompt to run in a chat session in the current working
+#                directory.
+#   serve-web    Run a server that displays the editor UI in browsers.
+#   tunnel       Make the current machine accessible from vscode.dev or other
+#                machines through a secure tunnel.
+
 
 # WP-CLI commands
 alias wppl='wp plugin list'
