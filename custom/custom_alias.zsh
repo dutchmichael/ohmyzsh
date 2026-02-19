e2d1c078b801c0f4d79671efbb47ccdda20a0baf
dutchmichael <m@dutchmichael.com>
2026-02-19 10:24:19 -0500

    updated custom aliases

--- custom/custom_alias.zsh
+++ custom/custom_alias.zsh
@@ -162,7 +162,7 @@
 
 # ghosttyy
   alias gtsc='ghostty +show-config --default --docs'
-  alias gtc='nvim /Users/dutchmichael/Library/Application\ Support/com.mithcelh.ghostty/config'
+  alias gtc='nvim /Users/dutchmichael/Library/Application\ Support/com.mitchellh.ghostty/config'
   alias gtkb='ghostty +list-keybinds --default'
 
 # Git
