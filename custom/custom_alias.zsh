7f19e2e98f150b3151a9f918a6de7c4fd94a7aba
dutchmichael <m@dutchmichael.com>
2026-02-26 14:23:55 -0500

    updated custom aliases

--- custom/custom_alias.zsh
+++ custom/custom_alias.zsh
@@ -528,33 +528,37 @@
 # Nodejs commands
   alias nd='echo "Running nodenv-doctor https://github.com/nodenv/nodenv-installer#nodenv-doctor"  && curl -fsSL https://raw.githubusercontent.com/nodenv/nodenv-installer/master/bin/nodenv-doctor | bash'
 
-# Notes
+# Notes Commands
   alias nth='notes --help'
-  # Notes Commands:
-    # Accounts 
-      alias ntsh='notes accounts --help'
-    # Add  
-      alias ntah='notes add --help'
-    # Cat  
-      alias ntch='notes cat --help' 
-    # Config  
-      alias ntcoh='notes config --help'
-    # Delete  
-      alias ntdth='note delete --help'
-    # Dump
-      alias ntdh='notes dump --help'
-    # Edit
-      alias gteh='notes edit --help' 
-    # List 
-      alias ntlh='notes list --help'
-    # Mkdir     Create a new folder.
-      alias ntdrh='notes mkdir --help'
-    # Move - Move a note to a different folder. 
-      alias ntmh='notes move --help'    
-    # Rename - Rename a note.
-      alias ntrh='notes rename --help' 
-    # Rmdir - Delete a folder.
-      alias ntdfh='notes rmdir --help'
+  # Accounts 
+    alias ntach='notes accounts --help'
+    alias ntac='notes accounts'
+  # Add  
+    alias ntah='notes add --help'
+  # Cat  
+    alias ntch='notes cat --help' 
+  # Config  
+    alias ntcoh='notes config --help'
+  # Delete  
+    alias ntdth='note delete --help'
+  # Dump
+    alias ntdh='notes dump --help'
+  # Edit
+    alias gteh='notes edit --help' 
+  # List 
+    alias ntlh='notes list --help'
+    alias ntla='notes list --account admin@pumc-salisbury.org'
+    alias ntld='notes list --account dutchmichael1@gmail.com'
+    alias ntli='notes list --account iCloud'
+    alias ntlih0='notes list --account iCloud h0.midapex.net'
+  # Mkdir - Create a new folder.
+    alias ntcfh='notes mkdir --help'
+  # Move - Move a note to a different folder. 
+    alias ntmh='notes move --help'    
+  # Rename - Rename a note.
+    alias ntrh='notes rename --help' 
+  # Rmdir - Delete a folder.
+    alias ntdfh='notes rmdir --help'
 
 # Oh-my-zsh
   alias omzu='omz update'
